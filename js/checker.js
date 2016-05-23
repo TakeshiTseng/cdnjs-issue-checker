aa72eadab57e7184bfb722e2dd21c1ceefbf90e8
Yi Tseng <a86487817@gmail.com>
2016-05-23 14:38:25 +0800

    Basic setup

---
+++ js/checker.js
@@ -0,0 +1,5 @@
+var prevLocation = '';
+let issueBasedUrl = "https://api.github.com/repos/cdnjs/cdnjs/issues/";
+let repoBaseUrl = "https://api.github.com/repos/";
+let GitHubRepoRegex = /https?:\/\/github.com\/[-a-zA-Z0-9_]+\/[-a-zA-Z0-9_]+/gi
+let npmRegex = /https?:\/\/www.npmjs.com\/package\/[-a-zA-Z0-9_]+/gi
