094f4512304c058f5fb8ac7f6d60b5fbdd46ce1e
Yi Tseng <a86487817@gmail.com>
2016-05-23 14:39:03 +0800

    init reloadPlugin function

--- js/checker.js
+++ js/checker.js
@@ -3,3 +3,16 @@ let issueBasedUrl = "https://api.github.com/repos/cdnjs/cdnjs/issues/";
 let repoBaseUrl = "https://api.github.com/repos/";
 let GitHubRepoRegex = /https?:\/\/github.com\/[-a-zA-Z0-9_]+\/[-a-zA-Z0-9_]+/gi
 let npmRegex = /https?:\/\/www.npmjs.com\/package\/[-a-zA-Z0-9_]+/gi
+
+
+var reloadPlugin = () => {
+}
+
+setInterval(() => {
+
+  if(window.location.pathname !== prevLocation) {
+    prevLocation = window.location.pathname;
+
+    reloadPlugin();
+  }
+}, 1000);
